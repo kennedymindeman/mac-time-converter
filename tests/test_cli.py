bfe07d4fbecc37ecb83018b41cb9ad98c21e00a6
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-29 22:25:42 -0500

    Change test name to test_epoch_utc_exits_without_error

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,6 @@ from src.mac_time_converter.cli import cli
 from click.testing import CliRunner
 
 
-def test_epoch_utc_output() -> None:
+def test_epoch_utc_exits_without_error() -> None:
     result = CliRunner().invoke(cli, ["to-datetime", "0", "--timezone", "UTC"])
     assert result.exit_code == 0
