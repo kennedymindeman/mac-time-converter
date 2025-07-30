360f17fff29e833bd04d09228f5ecba7b12903e2
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-29 22:32:49 -0500

    Add test for time in epoch result

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,3 +14,7 @@ def test_epoch_utc_exits_without_error(to_datetime_on_epoch_result) -> None:
 
 def test_epoch_utc_has_date_in_output(to_datetime_on_epoch_result) -> None:
     assert "1904-01-01" in to_datetime_on_epoch_result.output
+
+
+def test_epoch_utc_has_time_in_output(to_datetime_on_epoch_result) -> None:
+    assert "00:00:00" in to_datetime_on_epoch_result.output
