5f09919d56f42e7361fd6144b5320a76efa21557
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-29 20:57:39 -0500

    Change mac_time.py to match tests

--- src/mac_time_converter/mac_time.py
+++ src/mac_time_converter/mac_time.py
@@ -1,15 +1,11 @@
 from datetime import datetime, timedelta
-from dataclasses import dataclass
 
 
-class MacTime:
-    EPOCH = datetime(
-        year=1904,
-        month=1,
-        day=1,
-    )
+EPOCH = datetime(
+    year=1904,
+    month=1,
+    day=1,
+)
 
-def to_datetime(cls, seconds: float) -> datetime:
+def to_datetime(seconds: float) -> datetime:
     return EPOCH + timedelta(seconds=seconds)
-
-def from_data
