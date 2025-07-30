b88ee1c3dbb1ecd116a7d780fabf60a7621735fa
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-29 20:50:17 -0500

    Use EPOCH instead of epoch

--- tests/test_mac_time.py
+++ tests/test_mac_time.py
@@ -1,10 +1,7 @@
+import pytest
 from datetime import datetime
 from src.mac_time_converter.mac_time import MacTime
 
 
 def test_mac_to_datetime_at_epoch() -> None:
-    assert MacTime.to_datetime(0) == datetime(
-        year=1904,
-        month=1,
-        day=1,
-    )
+    assert MacTime.to_datetime(0) == MacTime.EPOCH
