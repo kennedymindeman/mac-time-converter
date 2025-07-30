5102476aff02e2f30cbf959f4b88d899f89427af
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-29 21:23:18 -0500

    Make tests specify timezone

--- tests/test_mac_time.py
+++ tests/test_mac_time.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import src.mac_time_converter.mac_time as mac_time
 
 
@@ -19,5 +19,6 @@ def test_round_trip() -> None:
         year=2022,
         month=2,
         day=2,
+        tzinfo=timezone.utc,
     )
     assert mac_time.to_datetime(mac_time.from_datetime(dt)) == dt
