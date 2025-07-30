863c66f5869a007a34cb4572e6cbf18506a7d958
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-29 21:24:23 -0500

    Specify utc time zone for epoch

--- src/mac_time_converter/mac_time.py
+++ src/mac_time_converter/mac_time.py
@@ -1,11 +1,13 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 
 EPOCH = datetime(
     year=1904,
     month=1,
     day=1,
+    tzinfo=timezone.utc,
 )
+
 
 def to_datetime(seconds: float) -> datetime:
     return EPOCH + timedelta(seconds=seconds)
