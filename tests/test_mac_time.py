4c22f27971a24f7f531c9c23e636162e402048dd
Kennedy Mindeman <kennedymindeman@gmail.com>
2025-07-29 20:58:47 -0500

    Add test for converting to mactime at epoch

--- tests/test_mac_time.py
+++ tests/test_mac_time.py
@@ -3,3 +3,7 @@ import src.mac_time_converter.mac_time as mac_time
 
 def test_mac_to_datetime_at_epoch() -> None:
     assert mac_time.to_datetime(0) == mac_time.EPOCH
+
+
+def test_datetime_to_mac_at_epoch() -> None:
+    assert mac_time.from_datetime(mac_time.EPOCH) == 0
